19e099114d7404db2cfcf82310a772f0e39ca216
MAS <digitalcolony@gmail.com>
2019-10-29 16:53:33 -0700

    table definition

---
+++ sql/cc_events_queue.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `cc_events_queue` (
+  `eventID` int(11) NOT NULL AUTO_INCREMENT,
+  `processed` int(11) NOT NULL,
+  PRIMARY KEY (`eventID`)
+) ENGINE = InnoDB AUTO_INCREMENT = 265757424 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
